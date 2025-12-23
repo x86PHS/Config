8763ba658af572d73a53c1204706022805fdfa9e
Petrvs <pethematician@gmail.com>
2025-12-23 17:58:03 -0300

    Editando arquivo

--- Ubuntu/22.04.5/config.sh
+++ Ubuntu/22.04.5/config.sh
@@ -1,34 +1,46 @@
-#!/bin/bash
+#########################################
+#	UBUNTU 22.04.5 - CONFIG SCRIPT	#
+#		23/12/2025		#
+#########################################
 
-# Essencials Tools
-apt install wget curl nc git make gcc net-tools libpcap-dev openvpn -y
+# Directories
+mkdir $(echo $USER)/configs
+cd $(echo $USER)/configs
+pwd -L
 
-# Essencials Tools II
-apt install mysql-common postgresql openssh-client tcpdump ssh -y
+# utility tools
+apt install git make gcc curl wget zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-mkdir ~/Config && cd ~/Config
+# net tools
+apt install net-tools libpcap-dev openvpn tcpdump wireshark-qt -y
 
-# Instalação Golang
+# Utility
+apt install apache2 postgresql mysql-common openssh-client ssh -y
+
+# GOLANG INSTALL
 wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
 rm -rf /usr/local/go && rm -rf /root/go && tar -C /root -xzf go1.24.2.linux-amd64.tar.gz
 export PATH=$PATH:/root/go/bin
 echo "PATH=\$PATH:/root/go/bin" >> /root/.bashrc && echo "PATH=\$PATH:/root/go/bin" >> /root/.zshrc
-mkdir ~/Config/Packages && mv go1* ~/Config/Packages/
+mkdir $(echo $USER)/configs/packages && mv go1* $(echo $USER)/configs/packages/
 
-
-# Python3 Install
+# PYTHON 3 INSTALL
 sudo apt-get install python3.9 python3-pip -y
 
-# Install Github Tools for Pentest
-mkdir ~/Config/Github && cd ~/Config/Github
-git clone https://github.com/nmap/nmap.git
-git clone https://github.com/OJ/gobuster.git && cd gobuster && go get && go build && go install
-apt install burpsuite -y
-mkdir /usr/share/wordlists/ && cd /usr/share/wordlists/ && git clone https://github.com/danielmiessler/SecLists.git
+# GITHUB Tools
 
-
-# Install Tools For Windows
-mkdir  ~/Config/WindowsResoures
-cd  ~/Config/WindowsResources
+# - Windows Resources
+mkdir /usr/share/WindowsResources
+cd /usr/share/WindowsResources
 git clone https://github.com/bitsadmin/wesng.git
 git clone https://gitlab.com/kalilinux/packages/windows-binaries.git && cd windows-binaries && rm -rf debian && wget https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe
+
+# - Wordlists
+mkdir /usr/share/wordlists/
+cd /usr/share/wordlists/
+git clone https://github.com/danielmiessler/SecLists.git
+mv SecLists/* .
+git clone https://github.com/00xZEROx00/kali-wordlists
+mv kali-wordlists/* .
+rm -rf SecLists kali-wordlists
